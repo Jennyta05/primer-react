811e23019bd5d5abb3f56c040604f6ad18ebdbe9
Jennyta05 <hinata.05@hotmail.com>
2022-01-24 11:13:49 -0500

    primer commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+import FormTodo from "./FormTodo";
+import TaskList from "./TaskList";
+
+
+const Container = () => {
+    const [list,setList]= useState([]);
+    
+    const handleAddItem = additem => {
+        setList([...list, additem]); 
+    };
+    return(
+        <div>
+            
+            <FormTodo handleAddItem ={handleAddItem}/>
+            
+            <TaskList list={list} setlist={setList}/>
+        </div>
+    );
+};
+
+export default Container;
